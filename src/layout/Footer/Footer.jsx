3112e2c93dbc98f2b399ed9cb29efa4d073e0177
Kousik Das <kousikdas02@gmail.com>
2024-04-11 20:49:37 +0530

    footer style

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -7,6 +7,7 @@ import Typography from "@mui/material/Typography";
 import CallIcon from '@mui/icons-material/Call';
 import EmailIcon from '@mui/icons-material/Email';
 import CustomButton from "../../ui/CustomButton/CustomButton"
+import FacebookIcon from "../../ui/icons/FacebookIcon"
 export const foooterSiteMaop = [
   {
     name: "Cabins",
@@ -24,57 +25,63 @@ export const foooterSiteMaop = [
     name: "Activities",
     path: "/activities",
   },
-  
+
 ]
 const Footer = () => {
 
   return (
     <FooterWrapperStyled>
       <Container fixed>
-      <Box className="footerTop">
-        <Box className="footerTopRow">
-          <Box className="footerTopCol">
-            <Link to="/" className='footerLogo'>
-              <img src={assets.logo} alt="header logo" />
-            </Link>
-          </Box>
-          <Box className="footerTopCol">
-            <Typography variant='body1' className="footerTitle">OUR LOCATION</Typography>
-            <Typography variant='body1'>1103 Maple St, Bandera TX, 78003</Typography>
-            <Link to="tel:1234567890"><CallIcon /> 1234567890</Link>
-            <Link to="mailto:test@gmail.com"><EmailIcon/> test@gmail.com</Link>
-          </Box>
-          <Box className="footerTopCol">
-            <Typography variant='body1' className="footerTitle">SITE MAP</Typography>
-            <List>
-              {
-                foooterSiteMaop.map((item, index) => {
-                  return (
-                    <ListItem key={index}>
-                      <Link to={item.path}>{item.name}</Link>
-                    </ListItem>
-                  )
-                })
-              }
-            </List>
-          </Box>
-          <Box className="footerTopCol">
-            <Typography variant='body1' className="footerTitle">HOURS</Typography>
-            <Typography variant='body1'>Our reservation and front desk team are available daily from 9am to 6pm CST.</Typography>
-            <CustomButton buttonType='white'>
-              BOOK ONLINE
-            </CustomButton>
+        <Box className="footerTop">
+          <Box className="footerTopRow">
+            <Box className="footerTopCol">
+              <Link to="/" className='footerLogo'>
+                <img src={assets.logo} alt="header logo" />
+              </Link>
+            </Box>
+            <Box className="footerTopCol">
+              <Typography variant='body1' className="footerTitle">OUR LOCATION</Typography>
+              <Typography variant='body1'>1103 Maple St, Bandera TX, 78003</Typography>
+              <List>
+                <ListItem>
+                  <Link to="tel:1234567890"><CallIcon /> 1234567890</Link>
+                </ListItem>
+                <ListItem>
+                  <Link to="mailto:test@gmail.com"><EmailIcon /> test@gmail.com</Link>
+                </ListItem>
+              </List>
+            </Box>
+            <Box className="footerTopCol">
+              <Typography variant='body1' className="footerTitle">SITE MAP</Typography>
+              <List>
+                {
+                  foooterSiteMaop.map((item, index) => {
+                    return (
+                      <ListItem key={index}>
+                        <Link to={item.path}>{item.name}</Link>
+                      </ListItem>
+                    )
+                  })
+                }
+              </List>
+            </Box>
+            <Box className="footerTopCol">
+              <Typography variant='body1' className="footerTitle">HOURS</Typography>
+              <Typography variant='body1'>Our reservation and front desk team are available daily from 9am to 6pm CST.</Typography>
+              <CustomButton buttonType='white'>
+                BOOK ONLINE
+              </CustomButton>
 
-            <List className='footerSocialList'>
-              <ListItem>
-                <Link to="#"></Link>
-              </ListItem>
-            </List>
+              <List className='footerSocialList'>
+                <ListItem disablePadding>
+                  <Link to="#"><FacebookIcon /></Link>
+                </ListItem>
+              </List>
+            </Box>
           </Box>
         </Box>
-      </Box>
 
-      <Box className="footerBot"></Box>
+        <Box className="footerBot"></Box>
 
       </Container>
     </FooterWrapperStyled>
--- src/styles/styledComponents/FooterWrapperStyled.jsx
+++ src/styles/styledComponents/FooterWrapperStyled.jsx
@@ -5,4 +5,21 @@ import { primaryColors } from "../../mui-theme/_muiPalette";
 
 export const FooterWrapperStyled = styled(Box)`
 background-color: ${primaryColors.black};
+
+.footerTop{
+    padding: 50px 0 0;
+        .footerTopRow{
+            display: flex;
+            flex-wrap: wrap;
+            .footerTopCol {
+                .footerLogo{
+                    width: 300px;
+                }
+                .footerTitle{
+                    color: ${primaryColors.white};
+                }
+            }
+        }
+}
+
 `
