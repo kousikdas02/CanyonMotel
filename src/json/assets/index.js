444794600ddec8209eebe0ce72fa67ce41de742e
unknown <sgmandaljj@gmail.com>
2024-04-11 22:27:09 +0530

    Rooms section

--- src/json/assets/index.js
+++ src/json/assets/index.js
@@ -1,5 +1,8 @@
 export const assets = {
     logo : "/assets/images/logo.png",
     propertyDetails : "/assets/images/propertyDetails.jpg",
+    rooms1 : "/assets/images/rooms1.jpg",
+    rooms2 : "/assets/images/rooms2.jpg",
+
 }
 
--- src/pages/PropertyDetails.jsx
+++ src/pages/PropertyDetails.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { PropertyDetailsWrapperStyled } from '../styles/styledComponents/PropertyDetailsWrapperStyled'
 import Wrapper from '../layout/Wrapper/Wrapper'
 import CommonInnerBanner from '../components/CommonInnerBanner/CommonInnerBanner'
+import OurRoom from '../components/OurRooms'
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
@@ -12,26 +13,26 @@ const PropertyDetails = () => {
       <PropertyDetailsWrapperStyled>
         <CommonInnerBanner/>
         
-          <Box className="after-banner-main-wrap">
+          <Box className="aftr-banr-prprty-dtls-wrap">
               <Box className="container">
                 <Box className="cmn_head">
                 <Typography variant='h2'>BANDERA'S FIRST AND ONLY RUSTIC BOUTIQUE MOTEL</Typography>
                 </Box>
-                <Grid className="after-banner-main-wrap" container spacing={2}>
+                <Grid className="after-banner-prprty-wrap" container spacing={2}>
                   <Grid item xs={6}>
-                      <Box className="common-sec-after-banner">
+                      <Box className="cmn-sec-aftr-prprty-banner">
                       <p>The Teton Court Motel is your clean, comfortable, affordable lodging option in Pinedale, Wyoming. A refurbished hotel near the center of Pinedale and just minutes from the Bridger Wilderness, the Wind River Mountain Range, and visiting the Museum of the Mountain Man. Other nearby options include exploring or boating on Fremont Lake, Green River Lake and Island Lake, fishing the Green River, or relaxing in the Granite Hot Springs.</p>
                       </Box>
                   </Grid>
                   <Grid item xs={6}>
-                    <Box className="common-sec-after-banner">
+                    <Box className="cmn-sec-aftr-prprty-banner">
                       <p>We are located on a quiet street near the center of Pinedale, within walking distance of restaurants, supermarkets, and bars. We are one block off US 191 as you go to/from Jackson (1.5 hours) and the Grand Teton National Park (2 hours) and south entrance of Yellowstone National Park (2.5 hours).</p>
                     </Box>
                   </Grid>
                 </Grid>
               </Box>
           </Box>
-
+          <OurRoom/>          
       </PropertyDetailsWrapperStyled>
     </Wrapper>
     
