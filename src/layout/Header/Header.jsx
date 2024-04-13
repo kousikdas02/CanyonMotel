67f654ed5f5cd10c30043e6062120931dd00f92c
Kousik Das <kousikdas02@gmail.com>
2024-04-14 05:11:17 +0530

    homepage responsive

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -2,17 +2,14 @@ import React, { useEffect, useState } from 'react'
 import { HeaderWrapperStyled } from '../../styles/styledComponents/HeaderWrapperStyled'
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import { Button, Container } from '@mui/material'
-import { Link, NavLink } from 'react-router-dom';
+import {  NavLink } from 'react-router-dom';
 import { assets } from '../../json/assets';
 const Header = () => {
   const drawerWidth = 240;
